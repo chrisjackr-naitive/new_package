263226b33c6faeeacf8c075fb555f0be8b8f4d31
Chris Richardson <chris.richardson@naitive.com>
2022-10-02 21:59:49 +0000

    find_packages)()

--- setup.py
+++ setup.py
@@ -15,10 +15,10 @@ setup(name = "new_package",
       author = "Naitive2022",
       description = "Short description...",
       long_description=read('README.md'), 
-      packages=find_packages(
+      packages=find_packages(),
                              #where='src',  # '.' by default
-                             include=['new_package*'],  # ['*'] by default
-                             exclude=['new_package.tests'],  # empty by default
-                            ),
+                            #  include=['new_package*'],  # ['*'] by default
+                            #  exclude=['new_package.tests'],  # empty by default
+                            # ),
       install_requires=[], 
      )
