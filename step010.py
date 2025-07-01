52813b8ee7396da54a08e25dddc87a3b5abcb866
chrisjackr-naitive <chris.richardson@naitive.com>
2025-07-01 14:57:20 +0100

    execute-osteosight updates

--- step010.py
+++ step010.py
@@ -1,8 +1,8 @@
 import time
 
 time.sleep(5)
-print("")
-print("\nLoading DICOM Image...")
-print("(✓) Hip DICOM loaded.")
-print("")
-print("\n <-- Continue processing -->")
+print("> ")
+print("> Loading DICOM Image...")
+print("> (✓) Hip DICOM loaded.")
+print("> ")
+print("> -- Continue processing --")
